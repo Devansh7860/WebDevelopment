451ceb5acd0ab53d9ab2659a4d10899132d6b967
DEVANSH BANSAL <71180891+Devansh7860@users.noreply.github.com>
2020-11-26 14:48:41 +0530

    assignment20 created

---
+++ Assignment20/assignment20.js
@@ -0,0 +1,9 @@
+
+let user = confirm("Are u the user?");
+console.log("Are u are the user:" , user);
+
+let name = prompt("What is your Full name?");
+console.log("Your full name is:" , name);
+
+let age = prompt("What is your age?" , 14);
+console.log("Your age is:" , age);
