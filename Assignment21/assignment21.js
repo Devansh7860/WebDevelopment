77be4f442cbcbe0a13c53e472b37444b5f06b1b5
DEVANSH BANSAL <71180891+Devansh7860@users.noreply.github.com>
2020-11-27 10:28:05 +0530

    assignment21 created

---
+++ Assignment21/assignment21.js
@@ -0,0 +1,15 @@
+let side = prompt("What is the side of square?");
+
+alert(`The area of square is: ${side * side}`);
+
+
+let age = prompt("What is your age?");
+alert(`Your age in days is roughly = ${age * 365}days `);
+
+let base = prompt("What is the base of the triangle?");
+let height = prompt("What is the height of the triangle?");
+alert(`Area of the triangle is =  ${1/2 * base * height}`);
+
+let perimeter = prompt("What is the perimeter of the circle?");
+alert(`The radius of the circle = ${perimeter / (44/7)}`);
+
